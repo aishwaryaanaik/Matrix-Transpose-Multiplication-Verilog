2b0950ffb85c1f97604f54116392684c2f5c7956
Aishwarya Naik <162492205+aishwaryaanaik@users.noreply.github.com>
2025-12-15 19:54:07 +0530

    Add testbench for 3x3 matrix multiplication

---
+++ Verilog_Codes/TestBench_3x3matrix.v
@@ -0,0 +1,20 @@
+module testtt;
+  // Inputs 
+reg [15:0] a; 
+// Outputs 
+wire [15:0] b; 
+wire [15:0] r; 
+// Instantiate the Unit Under Test (UUT) 
+multiplication uut ( 
+.a(a),  
+.b(b),  
+.r(r) 
+); 
+initial begin
+  // Initialize Inputs 
+a = 16'b0001001000010010; 
+// Wait 100 ns for global reset to finish 
+#100; 
+// Add stimulus here 
+end 
+endmodule 
